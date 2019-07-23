1b45e69759b8947d8db1288c581c7f92359e955a
Llf0703 <1667334662@qq.com>
2019-07-23 16:36:36 +0800

    add notice.py

---
+++ notice.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+# server酱推送
+# 调用方法：notice("sckey", "标题", "内容")
+
+import requests
+
+
+def notice(key, title, text):
+    url = 'https://sc.ftqq.com/'+key+'.send'
+    data = {
+        "text": title,
+        "desp": text
+    }
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+    }
+    req = requests.post(url, data=data, headers=headers)
+    print(req.content)
