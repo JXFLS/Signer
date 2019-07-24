4610c621da187f29bf61de697ed79db30b443aed
black_white_tony <blackwhitetony@outlook.com>
2019-07-24 11:23:28 +0800

    回显将输出至 signer.log, 错误信息将输出至 error.log

--- main.py
+++ main.py
@@ -48,30 +48,62 @@ def login(user):
 
 def main():
     users = get_user()
+    var = 0
+    bests = []
+    res = []
+    names = []
+    tot = 0
     for user in users:
         login(user)
         try:
             remain = wait.until(EC.visibility_of_element_located((By.ID, 'remain')))
         except TimeoutException:
-            with open('error.log','w') as ferr:
-                ferr.write("ERROR! " + user.Username() + ":Failed to login")
-            continue
+            with open('error.log','a') as ferr:
+                ferr.write("ERROR! " + user.Username() + ":Failed to login\n")
+        with open('signer.log', 'a') as l:
+            l.write(user.Username() + ':remain: ' + remain.text + '\n')
         try:
             button = work.find_element_by_id('checkin')
             button.click()
+            with open('signer.log', 'a') as l:
+                l.write(user.Username + ":check in successfully\n")
+            tot = tot + 1
+            record = work.find_element_by_id('msg').text
+            num = str(text[4:-6])
+            strnum = text[4:-6]
+            with open('signer.log', 'a') as l:
+                l.write(user.Username() + ":get " + strnum + '\n')
+            res.append(num)
+            names.append(user.Username())
+            if (var < num):
+                 var = num
         except NoSuchElementException:
             with open('error.log','w') as ferr:
-                ferr.write("warning:" + user.Username() + ":has checked in")
+                ferr.write("warning:" + user.Username() + ":has checked in\n")
         work.implicitly_wait(0.5)
         work.get(URL + "/user/logout")
         work.implicitly_wait(0.5)
     work.quit()
+    for i in range(len(names)):
+        if res[i] == var:
+            bests.append(names[i])
+    with open('signer.log', 'a') as l:
+        l.write("Today's best person/people is/are....")
+    if tot > 0:
+        for best in bests:
+            with open('signer.log', 'a') as l:
+                l.write(best + " ")
+        with open('signer.log', 'a') as l:
+            l.write("they got " + str(var) + '\n')
+    else:
+        with open('signer.log', 'a') as l:
+            l.write("No one! Lucky next time~~")
     return
 
 ff_options = webdriver.firefox.options.Options()
 ff_options.add_argument('--headless')
-work = webdriver.Firefox(options=ff_options)
-#work = webdriver.Firefox()
+#work = webdriver.Firefox(options=ff_options)
+work = webdriver.Firefox()
 wait = WebDriverWait(work, 20)
 
 if __name__ == '__main__':
@@ -82,5 +114,3 @@ if __name__ == '__main__':
             ferr.write(str(e))
     finally:
         work.quit()
-
-
